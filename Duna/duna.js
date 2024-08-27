f5d50df86199a2db413a2e53aab3a1e736650a74
sergiohbsantos201 <sergio.borges2002>
2024-08-27 19:27:34 -0300

    changes

--- Duna/duna.js
+++ Duna/duna.js
@@ -4,16 +4,25 @@ function duna_filmes() {
     document.getElementById('filmes').style.display="none";
     document.getElementById('livros').style.display="none";
     document.getElementById('dunaparte1').style.display="block";
-    document.getElementById('duna1984').style.display="block";
+    document.getElementById('duna1984_link').style.display="block";
     document.getElementById('dunaparte2').style.display="block";
 }
 
-function duna_paginainicial() {
-    document.getElementsById('pagina-inicial').style.display="block";
+function duna_paginainicial(){
     document.getElementById('duna_filmes').style.display="none";
-    document.getElementById('duna1984').style.display="none";
+    document.getElementById('pagina-inicial').style.display="block";
+    document.getElementById('filmes').style.display="none";
+    document.getElementById('livros').style.display="none";
     document.getElementById('dunaparte1').style.display="none";
+    document.getElementById('duna1984_link').style.display="none";
     document.getElementById('dunaparte2').style.display="none";
-    document.getElementById('filmes').style.display="block";
-    document.getElementById('livros').style.display="block";
+    document.getElementById("duna1984").style.display="none";
+}
+function duna1984page(){
+    document.getElementById("duna1984").style.display="block";
+    document.getElementById("duna1984_link").style.display="none";
+    document.getElementById('dunaparte1').style.display="none";
+    document.getElementById("dunaparte2").style.display="none";
+    document.getElementById("duna_filmes").style.display="none";
+
 }
